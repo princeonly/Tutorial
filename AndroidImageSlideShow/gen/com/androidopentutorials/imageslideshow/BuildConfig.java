19183d54496e2f8c9825daa6866ef7e5aa56e272
Prince <princeonly@gmail.com>
2020-07-05 17:29:00 +0530

    added "AndroidImageSlideShow"

---
+++ AndroidImageSlideShow/gen/com/androidopentutorials/imageslideshow/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.androidopentutorials.imageslideshow;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
